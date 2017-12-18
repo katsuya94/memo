1b2f48eeb2bb97eb5682dcc62a0c50b626ea4d3c
Adrien Tateno <adrien.k.tateno@gmail.com>
2017-12-18 12:25:39 -0700

    Add edit func

--- open.go
+++ open.go
@@ -3,10 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
 )
 
 var (
-	errorNotFound = errors.New("Memo not found")
+	errNotFound = errors.New("Memo not found")
 )
 
 func open(d date) error {
@@ -16,15 +19,15 @@ func open(d date) error {
 
 	raw, err := storage.retrive(d)
 
+	fmt.Println(raw)
+
 	if err == nil {
-		fmt.Println(raw)
-	} else if err == errorNotFound {
-		fmt.Println("")
+		return edit()
+	} else if err == errNotFound {
+		return edit()
 	} else {
 		return err
 	}
-
-	return nil
 }
 
 type storage interface {
@@ -35,9 +38,22 @@ type storage interface {
 type localStorage struct{}
 
 func (*localStorage) retrive(d date) (string, error) {
-	return "", errorNotFound
+	return "", errNotFound
 }
 
 func (*localStorage) store(d date, raw string) error {
 	return nil
+}
+
+func edit() error {
+	f, err := ioutil.TempFile("", "memo")
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("vim", f.Name())
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
 }
