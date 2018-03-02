39219eded470c33a8669a663f6788a0b9224e4b2
Adrien Tateno <adrien.k.tateno@gmail.com>
2018-03-02 15:53:50 -0600

    Remove unused unmarshal function

--- core/profile.go
+++ core/profile.go
@@ -1,29 +1,6 @@
 package core
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 type Profile struct {
 	PrimaryStorage   Storage
 	SecondaryStorage Storage
-}
-
-func (*Profile) UnmarshalJSON(b []byte) error {
-	type storageConfig struct {
-		Type    string
-		Options interface{}
-	}
-
-	type profileConfig struct {
-		PrimaryStorage   storageConfig
-		SecondaryStorage storageConfig
-	}
-
-	var config profileConfig
-	json.Unmarshal(b, &config)
-	fmt.Println(config.PrimaryStorage)
-	fmt.Println(config.SecondaryStorage)
-	return nil
 }
